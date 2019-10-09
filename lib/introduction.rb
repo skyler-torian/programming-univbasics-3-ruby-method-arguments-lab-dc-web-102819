2df6dc7fb443c18221c13451e2c4490ff3374261
Skyler Torian <skymanat7@yahoo.com>
2019-10-09 18:01:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,15 +7,24 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+introduction("Josh")
+introduction("Dan")
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
+introduction_with_language("Dan", "Ember.js")
+introduction_with_language("Josh", "React")
+
+
+
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+introduction_with_language_optional("Edwin", "Ruby")
+introduction_with_language_optional("Edwin", "Python")
 
 
 
